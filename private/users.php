d37e4009bd032d7e5165da8583cbf4c043e914c3
FynnHer <133028317+FynnHer@users.noreply.github.com>
2024-07-01 10:22:16 +0200

    Create users.php

---
+++ private/users.php
@@ -0,0 +1,7 @@
+<?php
+$users = [
+    'user1' => '$2b$10$FY4iZjuDv6AE4xt8lySP2.R2BQYdrZUQE2D3foDCTEDSsIC.6QMdm', // Hashes der Passwörter
+    'user2' => '$2y$10$kOzt.eR5r/yN5sIW5Y8EU.I13gG6bGhEAYO4/oEYRpA5p5xXOd5k6',
+    'user3' => '$2y$10$yQ4G8QvzpzLSU1Dmk5CZdeCpiX8IFWEjlbM8vF8cfN/EsybO8jJ1O'
+];
+?>
